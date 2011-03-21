03cf3af2288ef6646fae54264b30499c1a903ce8
Tomer Filiba <tomerf@il.ibm.com>
2011-03-21 18:32:03 +0200

    releasing 3.1.0

--- build.py
+++ build.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-import time
+import os
 import shutil
 import subprocess
 from optparse import OptionParser
+from rpyc.version import version_string
+
 
 def run(args, input = None, cwd = None, env = None, retcode = 0):
     proc = subprocess.Popen(args, stdin = subprocess.PIPE, stdout = subprocess.PIPE,
@@ -24,19 +26,17 @@ def main(publish = False):
     run(["python", "setup.py", "sdist", "--formats=zip,gztar"])
     run(["python", "setup.py", "bdist_wininst", "--plat-name=win32"])
     run(["python", "setup.py", "bdist_egg"])
+    # python 2.5 and 2.6 eggs
+    run(["c:\\python25\\python.exe", "setup.py", "bdist_egg"])
+    run(["c:\\python26\\python.exe", "setup.py", "bdist_egg"])
     
     if publish:
-        # tag in git
-        msg = "release %s (%s)" % (rpyc.version_string, time.asctime())
-        run(["git", "tag", "-a", "-f", "-m", msg, rpyc.version_string])
-        run(["git", "push", "--tag"])
-
         # publish on pypi
         run(["python", "setup.py", "register"])
         
         # upload to sourceforge
-        dst = "gangesmaster,rpyc@frs.sourceforge.net:/home/frs/project/r/rp/rpyc/%s/" % (rpyc.version_string,)
-        run(["scp", "-r", "dist", dst])
+        dst = "gangesmaster,rpyc@frs.sourceforge.net:/home/frs/project/r/rp/rpyc/main/%s/" % (version_string,)
+        run(["scp"] + ["dist/%s" % (f,) for f in os.listdir("dist")] + [dst])
 
     shutil.rmtree("build", ignore_errors = True)
     shutil.rmtree("RPyC.egg-info", ignore_errors = True)
