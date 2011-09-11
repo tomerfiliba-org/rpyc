e16b4b213e044459a197840594310162727635e6
Tomer Filiba <tomerfiliba@gmail.com>
2011-09-11 23:37:11 +0300

    issue #44: adding non-rpyc test

--- issues/issue44/nonrpyc-client.py
+++ issues/issue44/nonrpyc-client.py
@@ -4,12 +4,15 @@ import ssl
 for i in range(5000):
     if i % 100 == 0:
         print i
-    sock = ssl.wrap_socket(socket.socket(), server_side = False, keyfile = 'cert.key', 
-        certfile = 'cert.crt', cert_reqs = ssl.CERT_NONE, ca_certs = None, \
-        ssl_version = ssl.PROTOCOL_TLSv1)
+    sock = socket.socket()
     sock.connect(("localhost", 13388))
+    sock2 = ssl.wrap_socket(sock, server_side = False, keyfile = 'cert.key', 
+        certfile = 'cert.crt', cert_reqs = ssl.CERT_NONE, ca_certs = None, 
+        ssl_version = ssl.PROTOCOL_TLSv1)
+    
     for text in ["hello world", "foobar", "spam and eggs"]:
-        sock.send(text)
-        data = sock.recv(1000)
+        sock2.send(text)
+        data = sock2.recv(1000)
         assert data == text
+    #sock.close()
 
--- issues/issue44/nonrpyc-server.py
+++ issues/issue44/nonrpyc-server.py
@@ -1,22 +1,24 @@
 import socket
 import ssl
 import threading
+import time
 
 listener = socket.socket()
 listener.bind(("localhost", 13388))
 listener.listen(10)
 
-def handle_sock(sock):
+def handle_sock(s):
+    s2 = ssl.wrap_socket(s, server_side = True, keyfile = 'cert.key', certfile = 'cert.crt', 
+        cert_reqs = ssl.CERT_NONE, ca_certs = None, ssl_version = ssl.PROTOCOL_TLSv1)
     for i in range(3):
-        data = sock.recv(1000)
-        sock.send(data)
-    sock.close()
+        data = s2.recv(1000)
+        s2.send(data)
+    time.sleep(1)
+    #s2.close()
 
 while True:
     s, _ = listener.accept()
-    s2 = ssl.wrap_socket(s, server_side = True, keyfile = 'cert.key', certfile = 'cert.crt', 
-        cert_reqs = ssl.CERT_NONE, ca_certs = None, ssl_version = ssl.PROTOCOL_TLSv1)
-    t = threading.Thread(target = handle_sock, args = (s2,))
+    t = threading.Thread(target = handle_sock, args = (s,))
     t.start()
 
 
