b1b7fe89eb7bb5d6ffc37c13559ece38584af836
Tomer Filiba <tomerfiliba@gmail.com>
2011-09-11 23:37:11 +0300

    issue #44: adding non-rpyc test

--- issues/issue44/nonrpyc-client.py
+++ issues/issue44/nonrpyc-client.py
@@ -3,12 +3,13 @@ import ssl
 
 for i in range(5000):
     if i % 100 == 0:
-        print i,
+        print i
     sock = ssl.wrap_socket(socket.socket(), server_side = False, keyfile = 'cert.key', 
         certfile = 'cert.crt', cert_reqs = ssl.CERT_NONE, ca_certs = None, \
         ssl_version = ssl.PROTOCOL_TLSv1)
     sock.connect(("localhost", 13388))
-    sock.send("hello world")
-    data = sock.recv(1000)
-    assert data == "hello world"
+    for text in ["hello world", "foobar", "spam and eggs"]:
+        sock.send(text)
+        data = sock.recv(1000)
+        assert data == text
 
--- issues/issue44/nonrpyc-server.py
+++ issues/issue44/nonrpyc-server.py
@@ -3,19 +3,20 @@ import ssl
 import threading
 
 listener = socket.socket()
-listener = ssl.wrap_socket(listener, server_side = True, keyfile = 'cert.key', certfile = 'cert.crt', 
-    cert_reqs = ssl.CERT_NONE, ca_certs = None, ssl_version = ssl.PROTOCOL_TLSv1)
 listener.bind(("localhost", 13388))
 listener.listen(10)
 
 def handle_sock(sock):
-    data = sock.recv(1000)
-    sock.send(data)
+    for i in range(3):
+        data = sock.recv(1000)
+        sock.send(data)
     sock.close()
 
 while True:
     s, _ = listener.accept()
-    t = threading.Thread(target = handle_sock, args = (s,))
+    s2 = ssl.wrap_socket(s, server_side = True, keyfile = 'cert.key', certfile = 'cert.crt', 
+        cert_reqs = ssl.CERT_NONE, ca_certs = None, ssl_version = ssl.PROTOCOL_TLSv1)
+    t = threading.Thread(target = handle_sock, args = (s2,))
     t.start()
 
 
