0e92b2e8276de53018362d8b8e14de6885c472e7
Tomer Filiba <tomerfiliba@gmail.com>
2011-09-11 23:12:48 +0300

    issue #44: trying to reproduce bug

---
+++ issues/issue44/client.py
@@ -0,0 +1,28 @@
+import rpyc
+
+#
+# with explicit closing
+#
+for i in range(5000):
+    if i % 100 == 0:
+        print i,
+    c = rpyc.ssl_connect("localhost", 13388, keyfile = "cert.key", certfile = "cert.crt")
+    assert c.root.foo() == 18
+    c.close()
+
+print
+print "finished (1/2)"
+
+#
+# without explicit closing
+#
+for i in range(5000):
+    if i % 100 == 0:
+        print i,
+    c = rpyc.ssl_connect("localhost", 13388, keyfile = "cert.key", certfile = "cert.crt")
+    assert c.root.foo() == 18
+    #c.close()
+
+print
+print "finished (2/2)"
+
