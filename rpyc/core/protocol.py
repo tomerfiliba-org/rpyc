7abaf580fecd0d19ce1383559d1cf41c4688bf18
Gero Müller <post@geromueller.de>
2014-05-15 13:16:34 +0200

    fix sync_request in multi threading

--- rpyc/core/protocol.py
+++ rpyc/core/protocol.py
@@ -7,7 +7,7 @@ import itertools
 import socket
 import time
 
-from threading import Lock
+from threading import Lock, RLock, Event
 from rpyc.lib.compat import pickle, next, is_py3k, maxint, select_error
 from rpyc.lib.colls import WeakValueDict, RefCountingColl
 from rpyc.core import consts, brine, vinegar, netref
@@ -128,6 +128,9 @@ class Connection(object):
                   the connection. Default is True. If set to False, you will 
                   need to call :func:`_init_service` manually later
     """
+
+    SYNC_REQUEST_TIMEOUT = 30
+
     def __init__(self, service, channel, config = {}, _lazy = False):
         self._closed = True
         self._config = DEFAULT_CONFIG.copy()
@@ -140,6 +143,8 @@ class Connection(object):
         self._recvlock = Lock()
         self._sendlock = Lock()
         self._sync_replies = {}
+        self._sync_lock = RLock()
+        self._sync_event = Event()
         self._async_callbacks = {}
         self._local_objects = RefCountingColl()
         self._last_traceback = None
@@ -150,6 +155,8 @@ class Connection(object):
         if not _lazy:
             self._init_service()
         self._closed = False
+
+
     def _init_service(self):
         self._local_root.on_connect()
 
@@ -434,8 +441,22 @@ class Connection(object):
         :returns: the result of the request
         """
         seq = self._send_request(handler, args)
+
+        start_time = time.time()
         while seq not in self._sync_replies:
-            self.serve(0.1)
+            remaining_time = self.SYNC_REQUEST_TIMEOUT - (time.time() - start_time)
+            if remaining_time > self.SYNC_REQUEST_TIMEOUT:
+                raise socket.timeout
+
+            # lock or wait for signal
+            if self._sync_lock.acquire(False):
+                self._sync_event.clear()
+                self.serve(remaining_time)
+                self._sync_lock.release()
+                self._sync_event.set()
+            else:
+                self._sync_event.wait(remaining_time)
+
         isexc, obj = self._sync_replies.pop(seq)
         if isexc:
             raise obj
