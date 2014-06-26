20b33cf89ad1a49c892557acacdea126a5def917
Tomer Filiba <tomerfiliba@gmail.com>
2014-06-26 21:44:30 +0300

    realeasing 3.3

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from plumbum import local, cli
-from plumbum.utils import delete
+from plumbum.path.utils import delete
 from rpyc.version import version_string
 
 class Build(cli.Application):
--- rpyc/version.py
+++ rpyc/version.py
@@ -1,4 +1,4 @@
 version = (3, 3, 0)
 version_string = "3.3.0"
-release_date = "2013.07.01"
+release_date = "2014.06.27"
 
