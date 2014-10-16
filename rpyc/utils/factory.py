2258c1bd3d0f2e97b3e88c3d75a949ee09f0eac4
Tomer Filiba <tomerfiliba@gmail.com>
2014-10-16 10:57:15 +0300

    right-trim lines

--- rpyc/utils/factory.py
+++ rpyc/utils/factory.py
@@ -1,5 +1,5 @@
 """
-RPyC connection factories: ease the creation of a connection for the common 
+RPyC connection factories: ease the creation of a connection for the common
 cases)
 """
 import socket
@@ -30,7 +30,7 @@ class DiscoveryError(Exception):
 #------------------------------------------------------------------------------
 def connect_channel(channel, service = VoidService, config = {}):
     """creates a connection over a given channel
-    
+
     :param channel: the channel to use
     :param service: the local service to expose (defaults to Void)
     :param config: configuration dict
@@ -45,7 +45,7 @@ def connect_stream(stream, service = VoidService, config = {}):
     :param stream: the stream to use
     :param service: the local service to expose (defaults to Void)
     :param config: configuration dict
-    
+
     :returns: an RPyC connection
     """
     return connect_channel(Channel(stream), service = service, config = config)
@@ -53,12 +53,12 @@ def connect_stream(stream, service = VoidService, config = {}):
 def connect_pipes(input, output, service = VoidService, config = {}):
     """
     creates a connection over the given input/output pipes
-    
+
     :param input: the input pipe
     :param output: the output pipe
     :param service: the local service to expose (defaults to Void)
     :param config: configuration dict
-    
+
     :returns: an RPyC connection
     """
     return connect_stream(PipeStream(input, output), service = service, config = config)
@@ -66,10 +66,10 @@ def connect_pipes(input, output, service = VoidService, config = {}):
 def connect_stdpipes(service = VoidService, config = {}):
     """
     creates a connection over the standard input/output pipes
-    
+
     :param service: the local service to expose (defaults to Void)
     :param config: configuration dict
-    
+
     :returns: an RPyC connection
     """
     return connect_stream(PipeStream.from_std(), service = service, config = config)
@@ -77,7 +77,7 @@ def connect_stdpipes(service = VoidService, config = {}):
 def connect(host, port, service = VoidService, config = {}, ipv6 = False, keepalive = False):
     """
     creates a socket-connection to the given host and port
-    
+
     :param host: the hostname to connect to
     :param port: the TCP port to use
     :param service: the local service to expose (defaults to Void)
@@ -95,21 +95,21 @@ def ssl_connect(host, port, keyfile = None, certfile = None, ca_certs = None,
     """
     creates an SSL-wrapped connection to the given host (encrypted and
     authenticated).
-    
+
     :param host: the hostname to connect to
     :param port: the TCP port to use
     :param service: the local service to expose (defaults to Void)
     :param config: configuration dict
     :param ipv6: whether to create an IPv6 socket or an IPv4 one
-    
-    The following arguments are passed directly to 
+
+    The following arguments are passed directly to
     `ssl.wrap_socket <http://docs.python.org/dev/library/ssl.html#ssl.wrap_socket>`_:
-    
+
     :param keyfile: see ``ssl.wrap_socket``. May be ``None``
     :param certfile: see ``ssl.wrap_socket``. May be ``None``
     :param ca_certs: see ``ssl.wrap_socket``. May be ``None``
     :param cert_reqs: see ``ssl.wrap_socket``. By default, if ``ca_cert`` is specified,
-                      the requirement is set to ``CERT_REQUIRED``; otherwise it is 
+                      the requirement is set to ``CERT_REQUIRED``; otherwise it is
                       set to ``CERT_NONE``
     :param ssl_version: see ``ssl.wrap_socket``. The default is ``PROTOCOL_TLSv1``
     :param ciphers: see ``ssl.wrap_socket``. May be ``None``. New in Python 2.7/3.2
@@ -148,12 +148,12 @@ _ssh_connect_lock = threading.Lock()
 def ssh_connect(remote_machine, remote_port, service = VoidService, config = {}):
     """
     Connects to an RPyC server over an SSH tunnel (created by plumbum).
-    See `Plumbum tunneling <http://plumbum.readthedocs.org/en/latest/remote.html#tunneling>`_ 
+    See `Plumbum tunneling <http://plumbum.readthedocs.org/en/latest/remote.html#tunneling>`_
     for further details.
-    
+
     .. note::
        This function attempts to allocate a free TCP port for the underlying tunnel, but doing
-       so is inherently prone to a race condition with other processes who might bind the 
+       so is inherently prone to a race condition with other processes who might bind the
        same port before sshd does. Albeit unlikely, there is no sure way around it.
 
     :param remote_machine: an :class:`plumbum.remote.RemoteMachine` instance
@@ -173,14 +173,14 @@ def ssh_connect(remote_machine, remote_port, service = VoidService, config = {})
 def discover(service_name, host = None, registrar = None, timeout = 2):
     """
     discovers hosts running the given service
-    
+
     :param service_name: the service to look for
     :param host: limit the discovery to the given host only (None means any host)
     :param registrar: use this registry client to discover services. if None,
                       use the default UDPRegistryClient with the default settings.
     :param timeout: the number of seconds to wait for a reply from the registry
                     if no hosts are found, raises DiscoveryError
-    
+
     :raises: ``DiscoveryError`` if no server is found
     :returns: a list of (ip, port) pairs
     """
@@ -198,7 +198,7 @@ def discover(service_name, host = None, registrar = None, timeout = 2):
 
 def connect_by_service(service_name, host = None, service = VoidService, config = {}):
     """create a connection to an arbitrary server that exposes the requested service
-    
+
     :param service_name: the service to discover
     :param host: limit discovery to the given host only (None means any host)
     :param service: the local service to expose (defaults to Void)
@@ -213,7 +213,7 @@ def connect_by_service(service_name, host = None, service = VoidService, config 
 def connect_subproc(args, service = VoidService, config = {}):
     """runs an rpyc server on a child process that and connects to it over
     the stdio pipes. uses the subprocess module.
-    
+
     :param args: the args to Popen, e.g., ["python", "-u", "myfile.py"]
     :param service: the local service to expose (defaults to Void)
     :param config: configuration dict
@@ -225,9 +225,9 @@ def connect_subproc(args, service = VoidService, config = {}):
     return conn
 
 def connect_thread(service = VoidService, config = {}, remote_service = VoidService, remote_config = {}):
-    """starts an rpyc server on a new thread, bound to an arbitrary port, 
+    """starts an rpyc server on a new thread, bound to an arbitrary port,
     and connects to it over a socket.
-    
+
     :param service: the local service to expose (defaults to Void)
     :param config: configuration dict
     :param server_service: the remote service to expose (of the server; defaults to Void)
@@ -254,36 +254,36 @@ def connect_thread(service = VoidService, config = {}, remote_service = VoidServ
     return connect(host, port, service = service, config = config)
 
 def connect_multiprocess(service = VoidService, config = {}, remote_service = VoidService, remote_config = {}, args={}):
-    """starts an rpyc server on a new process, bound to an arbitrary port, 
+    """starts an rpyc server on a new process, bound to an arbitrary port,
     and connects to it over a socket. Basically a copy of connect_thread().
     However if args is used and if these are shared memory then changes
     will be bi-directional. That is we now have access to shared memmory.
-    
+
     :param service: the local service to expose (defaults to Void)
     :param config: configuration dict
     :param server_service: the remote service to expose (of the server; defaults to Void)
     :param server_config: remote configuration dict (of the server)
     :param args: dict of local vars to pass to new connection, form {'name':var}
-    
+
     Contributed by *@tvanzyl*
     """
     from multiprocessing import Process
-    
+
     listener = socket.socket()
     listener.bind(("localhost", 0))
     listener.listen(1)
-    
+
     def server(listener=listener, args=args):
         client = listener.accept()[0]
         listener.close()
-        conn = connect_stream(SocketStream(client), service = remote_service, config = remote_config)        
+        conn = connect_stream(SocketStream(client), service = remote_service, config = remote_config)
         try:
             for k in args:
                 conn._local_root.exposed_namespace[k] = args[k]
             conn.serve_all()
         except KeyboardInterrupt:
             interrupt_main()
-    
+
     t = Process(target = server)
     t.start()
     host, port = listener.getsockname()
