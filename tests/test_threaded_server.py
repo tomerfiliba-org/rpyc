1c59bd8d24644184148cf8bb0a4e164a2f85fd79
Thomas Gläßle <t_glaessle@gmx.de>
2017-07-28 11:31:13 +0200

    KISS test

--- tests/test_threaded_server.py
+++ tests/test_threaded_server.py
@@ -6,16 +6,11 @@ from rpyc import SlaveService
 import threading
 import unittest
 
-class BaseServerTest(object):
 
-    def _create_server(self):
-        raise NotImplementedError
-
-    def _create_client(self):
-        raise NotImplementedError
+class Test_ThreadedServer(unittest.TestCase):
 
     def setUp(self):
-        self.server = self._create_server()
+        self.server = ThreadedServer(SlaveService, port=18878, auto_register=False)
         self.server.logger.quiet = False
         t = threading.Thread(target=self.server.start)
         t.setDaemon(True)
@@ -26,7 +21,7 @@ class BaseServerTest(object):
         self.server.close()
 
     def test_connection(self):
-        c = self._create_client()
+        c = rpyc.classic.connect("localhost", port=18878)
         print( c.modules.sys )
         print( c.modules["xml.dom.minidom"].parseString("<a/>") )
         c.execute("x = 5")
@@ -34,23 +29,29 @@ class BaseServerTest(object):
         self.assertEqual(c.eval("1+x"), 6)
         c.close()
 
-class Test_ThreadedServer(BaseServerTest, unittest.TestCase):
-
-    def _create_server(self):
-        return ThreadedServer(SlaveService, port=18878, auto_register=False)
-
-    def _create_client(self):
-        return rpyc.classic.connect("localhost", port=18878)
-
-class Test_ThreadedServerOverUnixSocket(BaseServerTest, unittest.TestCase):
+class Test_ThreadedServerOverUnixSocket(unittest.TestCase):
 
     socket_path = tempfile.mktemp()
 
-    def _create_server(self):
-        return ThreadedServer(SlaveService, socket_path=self.socket_path, auto_register=False)
+    def setUp(self):
+        self.server = ThreadedServer(SlaveService, socket_path=self.socket_path, auto_register=False)
+        self.server.logger.quiet = False
+        t = threading.Thread(target=self.server.start)
+        t.setDaemon(True)
+        t.start()
+        time.sleep(0.5)
 
-    def _create_client(self):
-        return rpyc.classic.unix_connect(self.socket_path)
+    def tearDown(self):
+        self.server.close()
+
+    def test_connection(self):
+        c = rpyc.classic.unix_connect(self.socket_path)
+        print( c.modules.sys )
+        print( c.modules["xml.dom.minidom"].parseString("<a/>") )
+        c.execute("x = 5")
+        self.assertEqual(c.namespace["x"], 5)
+        self.assertEqual(c.eval("1+x"), 6)
+        c.close()
 
 if __name__ == "__main__":
     unittest.main()
