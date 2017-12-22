cc10351fd6099825c8b9ec61045113ae20833a1e
Thomas Gläßle <t_glaessle@gmx.de>
2017-12-22 17:10:50 +0100

    Start fixing rpycd…
    Resolves #231
    
    Fixes:
    
    - Remove absolute default paths for logfile and lockfile, that would
      prevent starting this as non-root (forcing to start this as root,
      defaulting to serve on 0.0.0.0, are you crazy btw?)
    
    - change CWD in DaemonContext back to dir that was active when first
      starting the process. This allows to find config files by relative
      path names…
    
    - always fallback on $RPYC/bin/rpycd.conf even if started elsewhere
    
    - bind to 127.0.0.1 by default
    
    - Actually call `serve_all` to make it do anything at all…

--- bin/rpycd.py
+++ bin/rpycd.py
@@ -4,6 +4,7 @@ import daemon
 import lockfile
 import sys
 import signal
+import os
 from rpyc.utils.server import ThreadedServer, ForkingServer
 from rpyc.core.service import SlaveService
 from rpyc.lib import setup_logger
@@ -13,12 +14,18 @@ except ImportError:
     from ConfigParser import ConfigParser
 
 server = None
+cur_dir = os.getcwd()
+bin_dir = os.path.dirname(__file__)
+
 
 def start():
     global server
 
     conf = ConfigParser()
-    conf.read('rpycd.conf')
+    conf.read([
+        os.path.join(cur_dir, 'rpycd.conf'),
+        os.path.join(bin_dir, 'rpycd.conf'),    # later files trump earlier ones
+    ])
 
     mode = conf.get("rpycd", "mode").lower()
     if mode == "threaded":
@@ -33,6 +40,7 @@ def start():
     server = factory(SlaveService, hostname = conf.get("rpycd", "host"),
         port = conf.getint("rpycd", "port"), reuse_addr = True)
     server.start()
+    server.serve_all()
 
 def reload(*args):
     server.close()
@@ -45,7 +53,7 @@ def stop(*args):
 
 if __name__ == "__main__":
     with daemon.DaemonContext(
-            pidfile = lockfile.FileLock('/var/run/rpydc.pid'),
+            working_directory = cur_dir,
+            pidfile = lockfile.FileLock('rpycd.pid'),
             signal_map = {signal.SIGTERM: stop, signal.SIGHUP: reload}):
         start()
-
