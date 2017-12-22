3320509bdf493eb4a3eca627df9d17986665fdad
Thomas Gläßle <t_glaessle@gmx.de>
2017-12-22 13:09:19 +0100

    Remove empty demo

--- demos/rpycfs/mount.rpycfs.py
+++
@@ -1,9 +0,0 @@
-# requires fuse bindings for python
-import fuse
-
-
-class RPyCFS(fuse.Fuse):
-    def __init__(self, conn, mountpoint):
-        self.conn = conn
-        fuse.Fuse.__init__(self, mountpoint)
-
