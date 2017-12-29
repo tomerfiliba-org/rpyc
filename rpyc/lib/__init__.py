aa0625f3fec40b15219d95fc360370e578a08feb
Thomas Gläßle <t_glaessle@gmx.de>
2017-12-29 14:11:57 +0100

    Move `hybridmethod` to `rpyc.lib`
    `rpyc.core` should not depend on `rpyc.utils` (danger of circular
    imports).

--- rpyc/lib/__init__.py
+++ rpyc/lib/__init__.py
@@ -40,3 +40,14 @@ def setup_logger(quiet = False, logfile = None):
         opts['filename'] = logfile
     logging.basicConfig(**opts)
 
+
+class hybridmethod(object):
+    """Decorator for hybrid instance/class methods that will act like a normal
+    method if accessed via an instance, but act like classmethod if accessed
+    via the class."""
+    def __init__(self, func):
+        self.func = func
+    def __get__(self, obj, cls):
+        return self.func.__get__(cls if obj is None else obj, obj)
+    def __set__(self, obj, val):
+        raise AttributeError("Cannot overwrite method")
