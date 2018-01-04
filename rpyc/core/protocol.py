a8468399f6b520f28c9f37a6c40c393c84b02cd3
Thomas Gläßle <t_glaessle@gmx.de>
2018-01-04 01:44:56 +0100

    Minor cleanup

--- rpyc/core/protocol.py
+++ rpyc/core/protocol.py
@@ -454,6 +454,8 @@ class Connection(object):
             self.close()
 
     def serve_threaded(self, thread_count=10):
+        """Serves all requests and replies for as long as the connection is
+        alive."""
         def _thread_target():
             try:
                 while True:
@@ -464,10 +466,6 @@ class Connection(object):
             except EOFError:
                 pass
 
-        threads = []
-
-        """Serves all requests and replies for as long as the connection is
-        alive."""
         try:
             threads = [spawn(_thread_target)
                        for _ in range(thread_count)]
