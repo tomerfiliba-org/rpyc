8b4d23d50897a117f19e138a838dae70950c7b2e
James Stronz <james@network-perception.com>
2019-12-03 02:31:13 -0600

    More unittests and best type behavior without refactoring BaseNetref

--- rpyc/lib/__init__.py
+++ rpyc/lib/__init__.py
@@ -162,8 +162,28 @@ def get_id_pack(obj):
     if hasattr(obj, '____id_pack__'):
         # netrefs are handled first since __class__ is a descriptor
         return obj.____id_pack__
+    elif inspect.ismodule(obj) or getattr(obj, '__name__', None) == 'module':
+        # TODO: not sure about this, need to enumerate cases in units
+        if isinstance(obj, type):  # module
+            obj_cls = type(obj)
+            name_pack = '{0}.{1}'.format(obj_cls.__module__, obj_cls.__name__)
+            return (name_pack, id(type(obj)), id(obj))
+        else:
+            if inspect.ismodule(obj) and obj.__name__ != 'module':
+                if obj.__name__ in sys.modules:
+                    name_pack = obj.__name__
+                else:
+                    name_pack = '{0}.{1}'.format(obj.__class__.__module__, obj.__name__)
+            elif inspect.ismodule(obj):
+                name_pack = '{0}.{1}'.format(obj__module__, obj.__name__)
+                print(name_pack)
+            elif hasattr(obj, '__module__'):
+                name_pack = '{0}.{1}'.format(obj.__module__, obj.__name__)
+            else:
+                obj_cls = type(obj)
+                name_pack = '{0}'.format(obj.__name__)
+            return (name_pack, id(type(obj)), id(obj))
     elif not inspect.isclass(obj):
-        # TODO: this is wrong for modules
         name_pack = '{0}.{1}'.format(obj.__class__.__module__, obj.__class__.__name__)
         return (name_pack, id(type(obj)), id(obj))
     else:
