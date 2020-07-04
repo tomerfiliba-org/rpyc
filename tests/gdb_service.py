b35c2392b5e1afe6777d293dedf996aab23de118
comrumino <comrumino@archstrike.org>
2020-07-04 02:22:00 -0500

    Showed that remote use of gdb is possible by adding test case to resolve #389

---
+++ tests/gdb_service.py
@@ -0,0 +1,17 @@
+import gdb
+import rpyc
+
+
+class GDBService(rpyc.Service):
+    def exposed_get(self):
+        return gdb
+
+    def exposed_quit(self):
+        gdb.execute('quit')
+
+
+if __name__ == "__main__":
+    server = rpyc.ThreadedServer(GDBService, port=0, protocol_config={'allow_all_attrs': True})
+    gdb.write(f'{server.port}\n')
+    gdb.flush()
+    server.start()
