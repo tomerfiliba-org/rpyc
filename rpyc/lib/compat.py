4479faaaa2802e987ac837c4d2b7435df1d09db0
comrumino <comrumino@archstrike.org>
2020-11-06 23:21:42 -0600

    Added is_py_gte37 to rpyc.lib.compat to allow skipping of unittests that require 3.7+ such as tests.test_dataclass

--- rpyc/lib/compat.py
+++ rpyc/lib/compat.py
@@ -7,6 +7,7 @@ import time
 
 is_py_3k = (sys.version_info[0] >= 3)
 is_py_gte38 = is_py_3k and (sys.version_info[1] >= 8)
+is_py_gte37 = is_py_3k and (sys.version_info[1] >= 7)
 
 
 if is_py_3k:
--- tests/test_dataclass.py
+++ tests/test_dataclass.py
@@ -1,15 +1,18 @@
 from __future__ import with_statement
 import rpyc
 import unittest
-from dataclasses import dataclass
-
-
-@dataclass
-class StdTypes:
-    exposed_intObj: int = 31
-    exposed_strObj: str = "stdtypes string"
-    exposed_floatObj: float = 3.14
-    exposed_bytesObj: bytes = b"stdtypes bytes"
+from rpyc.lib.compat import is_py_gte37
+if is_py_gte37:
+    from dataclasses import dataclass
+    @dataclass
+    class StdTypes(object):
+        exposed_intObj: int = 31
+        exposed_strObj: str = "stdtypes string"
+        exposed_floatObj: float = 3.14
+        exposed_bytesObj: bytes = b"stdtypes bytes"
+else:
+    class StdTypes(object):
+        pass
 
 
 class MyService(rpyc.Service):
@@ -17,7 +20,7 @@ class MyService(rpyc.Service):
     def exposed_create_dataclass(self):
         return StdTypes()
 
-
+@unittest.skipUnless(is_py_gte37, "Skipping since dataclasses is only in 3.7 and above")
 class TestRemoteDataclass(unittest.TestCase):
     def setUp(self):
         self.server = rpyc.utils.server.OneShotServer(MyService, port=0)
@@ -30,7 +33,7 @@ class TestRemoteDataclass(unittest.TestCase):
 
     def test_remote_dataclass(self):
         remote_dataclass = self.conn.root.create_dataclass()
-        self.assertEqual(31, remote_dataclass.intObj())
+        self.assertEqual(31, remote_dataclass.intObj)
         self.assertEqual("stdtypes string", remote_dataclass.strObj)
         self.assertEqual(3.14, remote_dataclass.floatObj)
         self.assertEqual(b"stdtypes bytes", remote_dataclass.bytesObj)
