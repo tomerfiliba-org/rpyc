b24257ffbcd424862a05d6ccfe832f7991a68fbe
comrumino <comrumino@archstrike.org>
2022-07-02 13:17:20 -0500

    Removed Test_Affinnity.test_unpinned since other unit tests provide the same coverage; Removal of test_unpinned simplifies the setUp for pinning to 0

--- tests/test_affinity.py
+++ tests/test_affinity.py
@@ -43,6 +43,7 @@ class Test_Affinity(unittest.TestCase):
         setattr(rpyc.core.protocol.Connection, '_dispatch', cls._orig_func)
 
     def setUp(self):
+        self._os.sched_setaffinity(0, {0, })
         self.conn = rpyc.connect_thread(rpyc.ClassicService, self.cfg, rpyc.ClassicService, self.cfg)
         self.bg_threads = [rpyc.BgServingThread(self.conn) for i in range(3)]
 
@@ -66,20 +67,11 @@ class Test_Affinity(unittest.TestCase):
         if self._os is not None:
             return self._os.sched_setaffinity(0, self._orig_affinity)
 
-    def test_unpinned(self):
-        """test without changing the default processor affinity"""
-        max_elapsed_time = self.cfg['sync_request_timeout']
-        elapsed_time = self._time_execute_sleep()
-        self.assertLess(elapsed_time, max_elapsed_time)
-        self.assertIn('count=0', repr(self.conn._recvlock))
-
     def test_pinned_to_0(self):
         """test behavior with processor affinity set such that this process is pinned to 0"""
         if self._skip:
             raise self._skip
         max_elapsed_time = self.cfg['sync_request_timeout']
-        self._os.sched_setaffinity(0, {0, })
         elapsed_time = self._time_execute_sleep()
-
         self.assertLess(elapsed_time, max_elapsed_time)
         self.assertIn('count=0', repr(self.conn._recvlock))
