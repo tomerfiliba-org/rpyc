868fd2de14d8183ec5d6ecb28a94a53e23a9acdd
comrumino <comrumino@archstrike.org>
2022-10-28 16:57:48 -0500

    Cleaned up module docstring for rpyc/__init__.py by removing ascii art and updating to py3 syntax

--- rpyc/__init__.py
+++ rpyc/__init__.py
@@ -1,43 +1,27 @@
-"""
-::
-
-         #####    #####             ####
-        ##   ##  ##   ##           ##             ####
-        ##  ##   ##  ##           ##                 #
-        #####    #####   ##   ##  ##               ##
-        ##  ##   ##       ## ##   ##                 #
-        ##   ##  ##        ###    ##              ###
-        ##   ##  ##        ##      #####
-     -------------------- ## ------------------------------------------
-                         ##
-
-Remote Python Call (RPyC)
+"""Remote Python Call (RPyC) is a transparent and symmetric distributed computing library
 Licensed under the MIT license (see `LICENSE` file)
-
-A transparent, symmetric and light-weight RPC and distributed computing
-library for python.
 
 Usage::
 
     >>> import rpyc
     >>> c = rpyc.connect_by_service("SERVICENAME")
-    >>> print c.root.some_function(1, 2, 3)
+    >>> print(c.root.some_function(1, 2, 3))
 
 Classic-style usage::
 
     >>> import rpyc
     >>> # `hostname` is assumed to be running a slave-service server
     >>> c = rpyc.classic.connect("hostname")
-    >>> print c.execute("x = 5")
+    >>> print(c.execute("x = 5"))
     None
-    >>> print c.eval("x + 2")
+    >>> print(c.eval("x + 2"))
     7
-    >>> print c.modules.os.listdir(".")       #doctest: +ELLIPSIS
+    >>> print(c.modules.os.listdir("."))       #doctest: +ELLIPSIS
     [...]
-    >>> print c.modules["xml.dom.minidom"].parseString("<a/>")   #doctest: +ELLIPSIS
+    >>> print(c.modules["xml.dom.minidom"].parseString("<a/>"))   #doctest: +ELLIPSIS
     <xml.dom.minidom.Document instance at ...>
     >>> f = c.builtin.open("foobar.txt", "rb")     #doctest: +SKIP
-    >>> print f.read(100)     #doctest: +SKIP
+    >>> print(f.read(100))    #doctest: +SKIP
     ...
 
 """
