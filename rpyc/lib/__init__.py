442fae133d4212c80aa145137309e63629bbf244
René Lehfeld <54720674+rlehfeld@users.noreply.github.com>
2024-04-27 15:06:28 +0200

    commit support for objects and classes without __class__ property

--- rpyc/lib/__init__.py
+++ rpyc/lib/__init__.py
@@ -170,44 +170,65 @@ def exp_backoff(collision):
 
 
 def get_id_pack(obj):
-    """introspects the given "local" object, returns id_pack as expected by BaseNetref
+    """introspects the given "local" object, returns id_pack as expected by
+    BaseNetref
 
-    The given object is "local" in the sense that it is from the local cache. Any object in the local cache exists
-    in the current address space or is a netref. A netref in the local cache could be from a chained-connection.
-    To handle type related behavior properly, the attribute `__class__` is a descriptor for netrefs.
+    The given object is "local" in the sense that it is from the local
+    cache. Any object in the local cache exists in the current address
+    space or is a netref. A netref in the local cache could be from a
+    chained-connection. To handle type related behavior properly, the
+    attribute `__class__` is a descriptor for netrefs.
 
-    So, check thy assumptions regarding the given object when creating `id_pack`.
+    So, check thy assumptions regarding the given object when creating
+    `id_pack`.
     """
-    if hasattr(obj, '____id_pack__'):
-        # netrefs are handled first since __class__ is a descriptor
-        return obj.____id_pack__
-    elif inspect.ismodule(obj) or getattr(obj, '__name__', None) == 'module':
-        # TODO: not sure about this, need to enumerate cases in units
+    undef = object()
+    name_pack = getattr(obj, '____id_pack__', undef)
+    if name_pack is not undef:
+        return name_pack
+
+    obj_name = getattr(obj, '__name__', None)
+    if (inspect.ismodule(obj) or obj_name == 'module'):
         if isinstance(obj, type):  # module
             obj_cls = type(obj)
-            name_pack = '{0}.{1}'.format(obj_cls.__module__, obj_cls.__name__)
+            name_pack = (
+                f'{obj_cls.__module__}.{obj_cls.__name__}'
+            )
             return (name_pack, id(type(obj)), id(obj))
-        else:
-            if inspect.ismodule(obj) and obj.__name__ != 'module':
-                if obj.__name__ in sys.modules:
-                    name_pack = obj.__name__
-                else:
-                    name_pack = '{0}.{1}'.format(obj.__class__.__module__, obj.__name__)
-            elif inspect.ismodule(obj):
-                name_pack = '{0}.{1}'.format(obj.__module__, obj.__name__)
-                print(name_pack)
-            elif hasattr(obj, '__module__'):
-                name_pack = '{0}.{1}'.format(obj.__module__, obj.__name__)
+
+        if inspect.ismodule(obj) and obj_name != 'module':
+            if obj_name in sys.modules:
+                name_pack = obj_name
             else:
-                obj_cls = type(obj)
-                name_pack = '{0}'.format(obj.__name__)
-            return (name_pack, id(type(obj)), id(obj))
-    elif not inspect.isclass(obj):
-        name_pack = '{0}.{1}'.format(obj.__class__.__module__, obj.__class__.__name__)
+                obj_cls = getattr(obj, '__class__', type(obj))
+                name_pack = (
+                    f'{obj_cls.__module__}.{obj_name}'
+                )
+        elif inspect.ismodule(obj):
+            name_pack = (
+                f'{obj.__module__}.{obj_name}'
+            )
+        else:
+            obj_module = getattr(obj, '__module__', undef)
+            if obj_module is not undef:
+                name_pack = (
+                    f'{obj.__module__}.{obj_name}'
+                )
+            else:
+                name_pack = obj_name
         return (name_pack, id(type(obj)), id(obj))
-    else:
-        name_pack = '{0}.{1}'.format(obj.__module__, obj.__name__)
-        return (name_pack, id(obj), 0)
+
+    if not inspect.isclass(obj):
+        obj_cls = getattr(obj, '__class__', type(obj))
+        name_pack = (
+            f'{obj_cls.__module__}.{obj_cls.__name__}'
+        )
+        return (name_pack, id(type(obj)), id(obj))
+
+    name_pack = (
+        f'{obj.__module__}.{obj_name}'
+    )
+    return (name_pack, id(obj), 0)
 
 
 def get_methods(obj_attrs, obj):
