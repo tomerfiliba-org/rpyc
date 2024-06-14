808676e55c2167353dd53eeffb7d783fd2fd32e9
René Lehfeld <54720674+rlehfeld@users.noreply.github.com>
2024-06-14 02:29:23 +0200

    revert change

--- rpyc/core/protocol.py
+++ rpyc/core/protocol.py
@@ -337,7 +337,8 @@ class Connection(object):
         if label == consts.LABEL_REMOTE_REF:
             id_pack = (str(value[0]), value[1], value[2])  # so value is a id_pack
             proxy = self._proxy_cache.get(id_pack)
-            if proxy is not None:
+            if id_pack in self._proxy_cache:
+                proxy = self._proxy_cache[id_pack]
                 proxy.____refcount__ += 1  # if cached then remote incremented refcount, so sync refcount
             else:
                 proxy = self._netref_factory(id_pack)
